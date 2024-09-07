1a9242c30326a57b8eba16ca3cbb9761d788794e
v1c999 <v1ct0riiaach0u@gmail.com>
2024-09-07 14:27:27 +0800

    stop names

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,36 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
   <header>
     <div class="title">BusChat!</div>
     <div class="stops">
       公車站名
+      <ul>
+        <li v-for="(stop, index) in stops" :key="index">{{ stop.name }}</li>
+      </ul>
     </div>
   </header>
 </template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      stops: [] // Initialize empty array for bus stops
+    };
+  },
+  mounted() {
+    // Fetch the JSON data from the public folder
+    axios.get('/json/GetStopLocation.json')
+      .then(response => {
+        this.stops = response.data.BusInfo; // Assign bus stop data to stops
+      })
+      .catch(error => {
+        console.error('Error fetching the JSON:', error);
+      });
+  }
+};
+</script>
 
 <style scoped>
 header {
@@ -19,11 +39,13 @@ header {
 }
 .title {
   font-size: 50px;
+  margin-bottom: 20px;
 }
-
+.stops ul {
+  padding-left: 20px;
+}
+.stops li {
+  margin-bottom: 10px;
+}
 </style>
 
-
-<script setup>
-
-</script>
